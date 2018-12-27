7051dba77c5580eadcfc200d45bcfc8a25c6ec18
SaraNaifar <naifarsara@gmail.com>
2018-12-27 12:19:09 +0100

    ajouter exceptions

--- src/main/java/com/ecommerce/microcommerce/model/Product.java
+++ src/main/java/com/ecommerce/microcommerce/model/Product.java
@@ -1,18 +1,23 @@
 package com.ecommerce.microcommerce.model;
 import com.fasterxml.jackson.annotation.JsonFilter;
 import com.fasterxml.jackson.annotation.JsonIgnore;
+import org.hibernate.validator.constraints.Length;
 
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.validation.constraints.Min;
 
 @Entity
 public class Product {
     @Id
     @GeneratedValue
     private int id ;
+
+    @Length(min=5, max=20, message = "\"Nom trop long ou trop court")
     private String nom;
+    @Min(value=1)
     private int prix;
     private int prixAchat;
 
--- src/main/java/com/ecommerce/microcommerce/web/controller/ProductController.java
+++ src/main/java/com/ecommerce/microcommerce/web/controller/ProductController.java
@@ -5,22 +5,28 @@ import com.ecommerce.microcommerce.model.Product;
 import com.fasterxml.jackson.databind.ser.FilterProvider;
 import com.fasterxml.jackson.databind.ser.impl.SimpleBeanPropertyFilter;
 import com.fasterxml.jackson.databind.ser.impl.SimpleFilterProvider;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 import org.springframework.http.converter.json.MappingJacksonValue;
+import com.ecommerce.microcommerce.exceptions.ProduitIntrouvableException;
 
+import javax.validation.Valid;
 import java.net.URI;
 import java.util.List;
 
 @RestController
+@Api(description="Api permet d'avoir les opérations CRUD de produit")
 public class ProductController {
     // on utilise @AutoWired pour créer une instance de productDao
     @Autowired
     private  ProductDao productDao ;
 
     //@TypeDeRequeteMapping permet de lier la requet avec la fonction à exécuter
+    @ApiOperation(value = "récupérer la list des produits")
     @GetMapping(value = "/Produits")
     public MappingJacksonValue listProduits(){
         Iterable<Product>  produits=  productDao.findAll();
@@ -38,24 +44,28 @@ public class ProductController {
 
         return produitsFiltres;
     }
-
+    @ApiOperation(value="recupérer les produits par son id")
     @GetMapping(value="Produits/{id}")
     public Product afficherUnProduit(@PathVariable int id) {
-        return productDao.findById(id);
+        Product produit =  productDao.findById(id);
+        if(produit==null ) throw new ProduitIntrouvableException("le produit avec l'id"+id+"n'existe pas");
+        return  produit;
     }
 
-    @GetMapping(value="test/Produits/{prixL}")
-    public List<Product> afficherProduitsPlusCher(@PathVariable int prixL){
+    @ApiOperation(value="récupérer les produits ayont le prix plus cher supérieur a 400")
+    @GetMapping(value="test/Produits/{prixLimit}")
+    public List<Product> afficherProduitsPlusCher(@PathVariable int prixLimit){
         return productDao.findByPrixGreaterThan(400);
     }
 
+    @ApiOperation(value="récupérer les produits par le nom recherché")
     @GetMapping(value="ProduitsParNom/{nomatrouve}")
     public List<Product> afficherProduitParNom(@PathVariable String nomatrouve){
         return productDao.findByNomLike("%"+nomatrouve+"%");
     }
 
    @PostMapping(value= "/Produits")
-    public ResponseEntity<Void> ajouterProduit(@RequestBody  Product product){
+    public ResponseEntity<Void> ajouterProduit(@Valid  @RequestBody  Product product){
         Product addedProduct = productDao.save(product);
         if( addedProduct == null){
             return ResponseEntity.noContent().build();
